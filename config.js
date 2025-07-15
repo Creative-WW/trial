b4563c81367dca007456aec46a2d7a3abb584412
Creative-WW <creative@wwdcomplete.com>
2025-07-15 10:18:48 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+window.projectConfig = {
+  title: "FLM Cooler Topper",
+  description: "View the FLM Cooler Topper in AR on your mobile device.",
+  image: "assets/CoolerTopper.png", // used for AR button
+  usdz: "assets/AR_DQ2049.usdz",
+  glb: "assets/AR_DQ2049.glb",
+  logo: "assets/dq_logo.png" // company logo
+};
