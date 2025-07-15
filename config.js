318f70b99f8a03267a6b725e84f0da08b612f1f3
Creative-WW <creative@wwdcomplete.com>
2025-07-15 10:19:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 window.projectConfig = {
-  title: "FLM Cooler Topper",
-  description: "View the FLM Cooler Topper in AR on your mobile device.",
-  image: "assets/CoolerTopper.png", // used for AR button
-  usdz: "assets/AR_DQ2049.usdz",
-  glb: "assets/AR_DQ2049.glb",
+  title: "Q Rack",
+  description: "View the Q Rack in AR on your mobile device.",
+  image: "assets/Q Rack.png", // used for AR button
+  usdz: "assets/AR_DQ3358.usdz",
+  glb: "assets/AR_DQ3358.glb",
   logo: "assets/dq_logo.png" // company logo
 };
